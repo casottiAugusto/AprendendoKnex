10a1cd43efd8da68916b1fd5393adc4f66d2e5ec
Joao PC Windos <augusto_casotti@hotmail.com>
2022-08-26 07:02:24 -0300

    Atualizando form

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const database =require("./database");
+const database = require("./database");
+
+let dados = [
+  {
+    nome: "Monster Hunter World",
+    preco: 65.40
+  },
+  {
+    nome: "Devil may cray",
+    preco: 30.50
+  },
+  {
+    nome:'WOW',
+   preco:120
+  }
+]
+let query = database.insert(dados).into("games").then(data => {
+  console.log(data)
+}).catch(err => {
+  console.log(err)
+});
